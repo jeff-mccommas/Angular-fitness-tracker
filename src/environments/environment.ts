d35fe476cb7dd755d705b8b477e8c648a368bcd1
jeff-mccommas <jeffmccommas@gmail.com>
2018-01-29 11:46:09 -0500

    optimize login

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDEgV6qo4scH4AjnRxjyx-Fvct-hicUUTA",
-    authDomain: "angular-fitness-tracker.firebaseapp.com",
-    databaseURL: "https://angular-fitness-tracker.firebaseio.com",
-    projectId: "angular-fitness-tracker",
-    storageBucket: "angular-fitness-tracker.appspot.com",
-    messagingSenderId: "535558611589"
+      apiKey: "AIzaSyDEgV6qo4scH4AjnRxjyx-Fvct-hicUUTA",
+      authDomain: "angular-fitness-tracker.firebaseapp.com",
+      databaseURL: "https://angular-fitness-tracker.firebaseio.com",
+      projectId: "angular-fitness-tracker",
+      storageBucket: "angular-fitness-tracker.appspot.com",
+      messagingSenderId: "535558611589"
   }
 };
